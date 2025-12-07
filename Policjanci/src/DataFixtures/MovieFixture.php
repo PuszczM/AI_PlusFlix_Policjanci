784359c56ed8216db00ec6568dee073c97797163
romandykyi <roman.dykyi.8210@gmail.com>
2025-12-07 22:02:39 +0100

    Update movie fixture

--- Policjanci/src/DataFixtures/MovieFixture.php
+++ Policjanci/src/DataFixtures/MovieFixture.php
@@ -11,27 +11,30 @@ class MovieFixture extends Fixture
     public function load(ObjectManager $manager): void
     {
         $movies = [
-            ['The Human Centipede', 'A mad scientist surgically joins people together.', 2009, "/img/posters/human_centipide.png", true],
-            ['Breaking Bad', 'A chemistry teacher turns to making drugs after cancer diagnosis.', 2008, "/img/posters/breaking_bad.png", false],
-            ['Better Call Saul', 'The story of Jimmy McGill becoming Saul Goodman.', 2015, "/img/posters/better_call_saul.png", false],
-            ['Inception', 'A skilled thief enters dreams to steal secrets.', 2010, "/img/posters/inception.png", false],
-            ['The Dark Knight', 'Batman faces the Joker in Gotham.', 2008, "/img/posters/dark_knight.png", false],
-            ['Interstellar', 'A team travels through a wormhole to save humanity.', 2014, "/img/posters/interstellar.png", false],
-            ['The Matrix', 'A hacker discovers reality is a simulation.', 1999, "/img/posters/matrix.png", false],
-            ['Pulp Fiction', 'Intersecting stories of crime in Los Angeles.', 1994, "/img/posters/pulp_fiction.png", true],
-            ['The Shawshank Redemption', 'A man wrongly imprisoned seeks hope and freedom.', 1994, "/img/posters/shawshank_redemption.png", false],
-            ['Stranger Things', 'Kids uncover mysterious events in their town.', 2016, "/img/posters/stranger_things.png", false],
+            // title, description, year, poster, isAdult, country, isSeries
+            ['The Human Centipede', 'A mad scientist surgically joins people together.', 2009, "/img/posters/human_centipide.png", true, 'Netherlands', false],
+            ['Breaking Bad', 'A chemistry teacher turns to making drugs after cancer diagnosis.', 2008, "/img/posters/breaking_bad.png", false, 'USA', true],
+            ['Better Call Saul', 'The story of Jimmy McGill becoming Saul Goodman.', 2015, "/img/posters/better_call_saul.png", false, 'USA', true],
+            ['Inception', 'A skilled thief enters dreams to steal secrets.', 2010, "/img/posters/inception.png", false, 'USA', false],
+            ['The Dark Knight', 'Batman faces the Joker in Gotham.', 2008, "/img/posters/dark_knight.png", false, 'USA', false],
+            ['Interstellar', 'A team travels through a wormhole to save humanity.', 2014, "/img/posters/interstellar.png", false, 'USA', false],
+            ['The Matrix', 'A hacker discovers reality is a simulation.', 1999, "/img/posters/matrix.png", false, 'USA', false],
+            ['Pulp Fiction', 'Intersecting stories of crime in Los Angeles.', 1994, "/img/posters/pulp_fiction.png", true, 'USA', false],
+            ['The Shawshank Redemption', 'A man wrongly imprisoned seeks hope and freedom.', 1994, "/img/posters/shawshank_redemption.png", false, 'USA', false],
+            ['Stranger Things', 'Kids uncover mysterious events in their town.', 2016, "/img/posters/stranger_things.png", false, 'USA', true],
         ];
 
-        foreach ($movies as [$title, $description, $year, $poster, $adult]) {
+        foreach ($movies as [$title, $description, $year, $poster, $adult, $country, $isSeries]) {
             $movie = new Movie();
             $movie->setTitle($title)
                 ->setDescription($description)
                 ->setReleaseYear($year)
                 ->setPosterPath($poster)
-                ->setIsAdult($adult);
-            $manager->persist($movie);
+                ->setIsAdult($adult)
+                ->setCountry($country)
+                ->setIsSeries($isSeries);
 
+            $manager->persist($movie);
             $this->addReference('movie_'.$title, $movie);
         }
 
