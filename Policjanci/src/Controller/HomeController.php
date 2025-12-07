51106bdd0bd4f0c8ce1c968a49b9af2212663fdf
romandykyi <roman.dykyi.8210@gmail.com>
2025-12-07 20:42:14 +0100

    Revert homepage

--- Policjanci/src/Controller/HomeController.php
+++ Policjanci/src/Controller/HomeController.php
@@ -2,10 +2,8 @@
 
 namespace App\Controller;
 
-
 use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,24 +11,16 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(Request $request, MovieRepository $movieRepository): Response
+    public function index(#[MapQueryParameter] ?string $prompt,
+                          MovieRepository $movieRepository): Response
     {
-        $searchTerm = $request->query->get('q');
         $movies = [];
-        if($searchTerm){
-            $queryBuilder = $movieRepository->createQueryBuilder('m')
-                ->where('m.title LIKE :term')
-                ->setParameter('term', '%' . $searchTerm . '%')
-                ->setMaxResults(20)
-                ->getQuery();
-            $movies = $queryBuilder->getResult();
-        }else{
-            $movies = $movieRepository->findBy([], null, 10);
-        }
+
+        $movies = $movieRepository->findMovies($prompt);
 
         return $this->render('home/index.html.twig', [
             'movies' => $movies,
-            'searchTerm' => $searchTerm,
+            'prompt' => $prompt,
         ]);
     }
 }
