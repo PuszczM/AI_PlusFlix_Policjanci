fb1883e3e7d3fb745c8acb9ed44cce4af88e9661
romandykyi <roman.dykyi.8210@gmail.com>
2025-12-08 21:46:36 +0100

    Add review repository

--- Policjanci/src/Controller/MovieController.php
+++ Policjanci/src/Controller/MovieController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Movie;
 use App\Repository\MovieRepository;
+use App\Repository\ReviewRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class MovieController extends AbstractController
 {
     #[Route('/movie/{id}', name: 'movie_show')]
-    public function show(Movie $movie): Response
+    public function show(Movie $movie, ReviewRepository $reviewRepository): Response
     {
+        $reviews = $reviewRepository->findBy(['movie' => $movie]);
+
         return $this->render('movie/show.html.twig', [
-            'movie' => $movie
+            'movie' => $movie,
+            'reviews' => $reviews
         ]);
     }
 }
