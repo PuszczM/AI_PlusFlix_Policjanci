1fe68fac1a1072205ca328908aa6dfb78c8f0a9d
romandykyi <roman.dykyi.8210@gmail.com>
2025-12-08 21:46:36 +0100

    Add review repository

---
+++ Policjanci/src/Repository/ReviewRepository.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Review;
+use App\Entity\Movie;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\Exception\ORMException;
+use Doctrine\ORM\OptimisticLockException;
+
+class ReviewRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Review::class);
+    }
+
+    public function addReviewTransactional(Movie $movie, Review $review): void
+    {
+        $em = $this->getEntityManager();
+
+        $em->getConnection()->beginTransaction();
+
+        try {
+            $review->setMovie($movie);
+
+            $movie->setAllReviewsCount($movie->getAllReviewsCount() + 1);
+
+            if ($review->isPositive()) {
+                $movie->setPositiveReviewsCount($movie->getPositiveReviewsCount() + 1);
+            }
+
+            $em->persist($review);
+            $em->persist($movie);
+
+            $em->flush();
+
+            $em->getConnection()->commit();
+        } catch (\Exception $e) {
+            $em->getConnection()->rollBack();
+            throw $e;
+        }
+    }
+}
