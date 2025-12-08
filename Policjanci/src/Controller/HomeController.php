35cc1ac84f3a049098d1c0d20f15acfc5a9f7cc9
romandykyi <roman.dykyi.8210@gmail.com>
2025-12-08 15:11:56 +0100

    Fix service icons retrieval

--- Policjanci/src/Controller/HomeController.php
+++ Policjanci/src/Controller/HomeController.php
@@ -6,6 +6,7 @@ use App\Filter\MovieFilter;
 use App\Filter\MovieTypeFilter;
 use App\Repository\MovieRepository;
 use App\Repository\CategoryRepository;
+use App\Repository\ServiceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
@@ -25,9 +26,11 @@ class HomeController extends AbstractController
         #[MapQueryParameter] ?int $minScore = null,
         #[MapQueryParameter] ?int $maxScore = null,
         #[MapQueryParameter] ?string $type = null,
-        MovieRepository $movieRepository
+        MovieRepository $movieRepository,
+        ServiceRepository $serviceRepository
     ): Response
     {
+        $availableServices = $serviceRepository->findAllOrdered();
         // 🔥 tłumaczenie stringa "film" / "series" -> enum
         $movieType = $type ? MovieTypeFilter::from($type) : null;
 
@@ -58,6 +61,7 @@ class HomeController extends AbstractController
             'minScore' => $filter->minScore,
             'maxScore' => $filter->maxScore,
             'type' => $type,
+            'availableServices' => $availableServices
         ]);
     }
 
@@ -74,9 +78,11 @@ class HomeController extends AbstractController
         #[MapQueryParameter] ?int $maxScore = null,
         #[MapQueryParameter] ?MovieTypeFilter $type = null,
         MovieRepository $movieRepository,
-        CategoryRepository $categoryRepository
+        CategoryRepository $categoryRepository,
+        ServiceRepository $serviceRepository
     ): Response
     {
+        $availableServices = $serviceRepository->findAllOrdered();
         $filter = new MovieFilter(
             $prompt,
             $categories ? array_map('trim', explode(',', $categories)) : [],
@@ -91,23 +97,23 @@ class HomeController extends AbstractController
         );
 
         return $this->render('movie/search.html.twig', [
-            'movies'            => $movieRepository->findMoviesByFilter($filter),
-            'prompt'            => $prompt,
-            'categories'        => $categoryRepository->findAll(),
-            'selectedCategories'=> $filter->categories,
+            'movies' => $movieRepository->findMoviesByFilter($filter),
+            'prompt' => $prompt,
+            'categories' => $categoryRepository->findAll(),
+            'selectedCategories' => $filter->categories,
 
-            'services'          => $filter->services,
-            'isRated18'         => $filter->isRated18,
-            'yearAfter'         => $filter->yearAfter,
-            'yearBefore'        => $filter->yearBefore,
-            'country'           => $filter->country,
-            'minScore'          => $filter->minScore,
-            'maxScore'          => $filter->maxScore,
+            'services' => $filter->services,
+            'isRated18' => $filter->isRated18,
+            'yearAfter' => $filter->yearAfter,
+            'yearBefore' => $filter->yearBefore,
+            'country' => $filter->country,
+            'minScore' => $filter->minScore,
+            'maxScore' => $filter->maxScore,
 
             // 🔥 always string for Twig
-            'type'              => $filter->movieType?->value,
+            'type' => $filter->movieType?->value,
+
+            'availableServices' => $availableServices
         ]);
     }
-
-
 }
