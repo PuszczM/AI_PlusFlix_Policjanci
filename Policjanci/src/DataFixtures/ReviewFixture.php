a156cd3170bb47b0f250bcd4d059ee5de3dd3649
romandykyi <roman.dykyi.8210@gmail.com>
2025-12-08 22:37:20 +0100

    Update review fixtures

--- Policjanci/src/DataFixtures/ReviewFixture.php
+++ Policjanci/src/DataFixtures/ReviewFixture.php
@@ -13,35 +13,73 @@ class ReviewFixture extends Fixture implements DependentFixtureInterface
     public function load(ObjectManager $manager): void
     {
         $reviews = [
-            ['Breaking Bad', true, 'Walter White cooked Heisenburger.'],
-            ['Breaking Bad', true, 'Vravo Bince!'],
+            ['Breaking Bad', true, 'Walter White cooked Heisenburger.', null],
+            ['Breaking Bad', true, 'Vravo Bince!', 'skong_enjoyer'],
+            ['Breaking Bad', true, null, null],
+            ['Breaking Bad', true, null, null],
+            ['Breaking Bad', true, null, null],
+            ['Breaking Bad', true, null, null],
+            ['Breaking Bad', false, null, null],
 
-            ['Better Call Saul', true, 'I would let Saul Goodman represent me even if I was guilty. Especially if I was guilty.'],
-            ['Better Call Saul', true, 'Best CRIMINAL lawyer ever'],
+            ['Better Call Saul', true, 'I would let Saul Goodman represent me even if I was guilty. Especially if I was guilty.', null],
+            ['Better Call Saul', true, 'Best CRIMINAL lawyer ever', 'pinkguy'],
+            ['Better Call Saul', true, null, null],
+            ['Better Call Saul', false, null, null],
 
-            ['The Matrix', true, 'The Matrix is the most accidentally-trans-then-intentionally-trans masterpiece ever. Slay, Neo!'],
-            ['The Matrix', true, 'This movie invented red pills BEFORE the internet ruined the metaphor'],
+            ['The Matrix', true, 'The Matrix is the most accidentally-trans-then-intentionally-trans masterpiece ever. Slay, Neo!', 'sophie21'],
+            ['The Matrix', true, 'This movie invented red pills BEFORE the internet ruined the metaphor', 'nic3guy'],
+            ['The Matrix', true, null, null],
+            ['The Matrix', true, null, null],
+            ['The Matrix', false, null, null],
 
-            ['The Dark Knight', true, 'We live in a society% world record attempt.'],
-            ['The Dark Knight', false, 'I tried to become the Joker after watching this. Did not work. Landlord still wants rent...'],
+            ['The Dark Knight', true, 'We live in a society% world record attempt.', 'skong_enjoyer'],
+            ['The Dark Knight', false, 'I tried to become the Joker after watching this. Did not work. Landlord still wants rent...', 'joeduh'],
+            ['The Dark Knight', true, null, null],
+            ['The Dark Knight', true, null, null],
+            ['The Dark Knight', true, null, null],
+            ['The Dark Knight', true, null, null],
+            ['The Dark Knight', false, null, null],
+            ['The Dark Knight', false, null, null],
+            ['The Dark Knight', false, null, null],
+            ['The Dark Knight', false, null, null],
+            ['The Dark Knight', false, null, null],
 
-            ['Inception', true, 'Was dreaming that I understood the plot. Woke up confused again 10/10'],
+            ['Inception', true, 'Was dreaming that I understood the plot. Woke up confused again 10/10', null],
 
-            ['Interstellar', true, 'Cried at the bookshelf scene'],
-            ['Interstellar', false, 'No amogus :('],
+            ['Interstellar', true, 'Cried at the bookshelf scene', 'roxie2008'],
+            ['Interstellar', false, 'No amogus :(', 'skong_enjoyer'],
+            ['Interstellar', true, null, null],
+            ['Interstellar', true, null, null],
+            ['Interstellar', false, null, null],
 
-            ['Pulp Fiction', false, 'I did not watch this'],
+            ['Pulp Fiction', false, 'I did not watch this', 'skong_enjoyer'],
+            ['Pulp Fiction', true, null, null],
+            ['Pulp Fiction', true, null, null],
+            ['Pulp Fiction', true, null, null],
+            ['Pulp Fiction', true, null, null],
+            ['Pulp Fiction', true, null, null],
+            ['Pulp Fiction', true, null, null],
 
-            ['Stranger Things', true, 'Kids fighting monsters while adults argue. Peak parenting.'],
-            ['Stranger Things', false, 'Ohio irl is better'],
+            ['Stranger Things', true, 'Kids fighting monsters while adults argue. Peak parenting.', 'brah69'],
+            ['Stranger Things', false, 'Ohio irl is better', 'skong_enjoyer'],
+            ['Stranger Things', true, null, null],
+            ['Stranger Things', true, null, null],
+            ['Stranger Things', false, null, null],
+            ['Stranger Things', false, null, null],
+            ['Stranger Things', false, null, null],
 
-            ['The Human Centipede', false, 'I wish I could uninstall this movie from my brain'],
-            ['The Human Centipede', false, 'Woke'],
+            ['The Human Centipede', false, 'I wish I could uninstall this movie from my brain', 'soyboy'],
+            ['The Human Centipede', false, 'Woke', 'trump2028'],
+            ['The Human Centipede', false, null, null],
+            ['The Human Centipede', false, null, null],
+            ['The Human Centipede', false, null, null],
+            ['The Human Centipede', false, null, null],
+            ['The Human Centipede', false, null, null],
         ];
 
         $counters = [];
 
-        foreach ($reviews as [$movieName, $isPositive, $comment]) {
+        foreach ($reviews as [$movieName, $isPositive, $comment, $username]) {
             /** @var Movie $movie */
             $movie = $this->getReference('movie_'.$movieName, Movie::class);
 
@@ -62,7 +100,8 @@ class ReviewFixture extends Fixture implements DependentFixtureInterface
             $review = new Review();
             $review->setMovie($movie)
                 ->setIsPositive($isPositive)
-                ->setComment($comment);
+                ->setComment($comment)
+                ->setUsername($username);
 
             $manager->persist($review);
         }
