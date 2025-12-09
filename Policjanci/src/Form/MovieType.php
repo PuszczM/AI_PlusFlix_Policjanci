697f4f9287c6744c1f75f4c5a61fd75fd45bb06c
romandykyi <roman.dykyi.8210@gmail.com>
2025-12-10 00:37:35 +0100

    Implement film addition

--- Policjanci/src/Form/MovieType.php
+++ Policjanci/src/Form/MovieType.php
@@ -10,7 +10,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -18,64 +18,59 @@ class MovieType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        /** @var Category[] $categories */
+        $categories = $options['categories'];
+        /** @var Service[] $services */
+        $services = $options['services'];
+
         $builder
             ->add('title', TextType::class, [
-                'label' => false,
-                'attr' => [
-                    'placeholder' => 'Movie title',
-                    'maxlength' => MovieDTO::TITLE_SIZE,
-                ],
+                'label' => 'Title',
+                'attr' => ['maxlength' => MovieDTO::TITLE_SIZE, 'placeholder' => 'Movie title'],
             ])
             ->add('description', TextareaType::class, [
-                'label' => false,
+                'label' => 'Description',
                 'required' => false,
-                'attr' => [
-                    'placeholder' => 'Description...',
-                    'maxlength' => MovieDto::DESCRIPTION_SIZE,
-                ],
+                'attr' => ['maxlength' => MovieDTO::DESCRIPTION_SIZE, 'placeholder' => 'Movie description...'],
             ])
             ->add('releaseYear', IntegerType::class, [
-                'label' => false,
-                'attr' => [
-                    'placeholder' => 'Release year',
-                ],
+                'label' => 'Release Year',
+                'attr' => ['placeholder' => '2025'],
             ])
             ->add('country', TextType::class, [
-                'label' => false,
-                'attr' => [
-                    'placeholder' => 'Country',
-                    'maxlength' => MovieDTO::COUNTRY_SIZE,
-                ],
+                'label' => 'Country',
+                'attr' => ['maxlength' => MovieDTO::COUNTRY_SIZE, 'placeholder' => 'Country'],
             ])
-            ->add('isSeries', CheckboxType::class, [
-                'label' => 'Is it a series?',
-                'required' => false,
+            ->add('isSeries', ChoiceType::class, [
+                'label' => 'Is this a series?',
+                'choices' => ['Yes' => true, 'No' => false],
+                'expanded' => true,
+                'multiple' => false,
             ])
             ->add('posterPath', TextType::class, [
-                'label' => false,
+                'label' => 'Poster Path',
                 'required' => false,
-                'attr' => [
-                    'placeholder' => 'Poster path (optional)',
-                    'maxlength' => MovieDTO::POSTER_PATH_SIZE,
-                ],
+                'attr' => ['maxlength' => MovieDTO::POSTER_PATH_SIZE, 'placeholder' => '/img/poster.png'],
             ])
-            ->add('isAdult', CheckboxType::class, [
-                'label' => '18+ content',
-                'required' => false,
+            ->add('isAdult', ChoiceType::class, [
+                'label' => '18+ Content?',
+                'choices' => ['Yes' => true, 'No' => false],
+                'expanded' => true,
+                'multiple' => false,
             ])
-
             ->add('categories', EntityType::class, [
                 'class' => Category::class,
+                'choices' => $categories,
                 'choice_label' => 'name',
-                'expanded' => true,
+                'expanded' => true, // checkboxes
                 'multiple' => true,
                 'label' => 'Categories',
             ])
-
             ->add('services', EntityType::class, [
                 'class' => Service::class,
+                'choices' => $services,
                 'choice_label' => 'fullName',
-                'expanded' => true,
+                'expanded' => true, // checkboxes
                 'multiple' => true,
                 'label' => 'Available on',
             ]);
@@ -85,6 +80,10 @@ class MovieType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => MovieDTO::class,
+            'categories' => [],
+            'services' => [],
         ]);
+
+        $resolver->setRequired(['categories', 'services']);
     }
 }
